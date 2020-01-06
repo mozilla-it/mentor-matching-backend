0f8e9d75d6148ea638c8228810495dd8c6127293
ophie200 <ophie200@users.noreply.github.com>
2020-01-06 11:53:00 -0800

    Create ranking.py

---
+++ ranking.py
@@ -0,0 +1,11 @@
+
+class Ranker():
+	
+	def rank_mentors(learner):
+		# for learner, update all available mentor rankings in learner_mentor_ranking table
+		return 100
+		
+	def rank_learners(mentor):
+		# for mentor, update all available learner rankings in mentor_learner_ranking table
+		return 100
+
