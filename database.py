411af79af9701841bc7abbc1b0fb969c1517975e
ophie200 <ophie200@users.noreply.github.com>
2020-01-06 12:19:20 -0800

    Update database.py

--- database.py
+++ database.py
@@ -27,23 +27,3 @@ def init_db():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
-    # Create the fixtures
-#     engineering = Department(name='Engineering')
-#     db_session.add(engineering)
-#     hr = Department(name='Human Resources')
-#     db_session.add(hr)
-# 
-#     manager = Role(name='manager')
-#     db_session.add(manager)
-#     engineer = Role(name='engineer')
-#     db_session.add(engineer)
-# 
-#     peter = Employee(name='Peter', department=engineering, role=engineer)
-#     db_session.add(peter)
-#     roy = Employee(name='Roy', department=engineering, role=engineer)
-#     db_session.add(roy)
-#     tracy = Employee(name='Tracy', department=hr, role=manager)
-#     db_session.add(tracy)
-#     db_session.commit()
-
-	
